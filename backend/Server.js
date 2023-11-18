b66101f39737ab89c57010934935ce12c2994fa6
jos77we <josemaina207@gmail.com>
2023-11-18 19:09:02 +0300

    Allowed access-control-allow-origin

--- backend/Server.js
+++ backend/Server.js
@@ -11,6 +11,12 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://loquacious-vacherin-5d89b3.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use('/api', require('./routes/ImagePosterRoute'))
 app.use('/product', require('./routes/ProductsRoute'))
 app.use('/api/user', require('./routes/UserRoute'))
